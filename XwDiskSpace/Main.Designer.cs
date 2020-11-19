c709d5ccc4b15a777c39395b2dd72af5097f212a
Max <Maximiano.Faustino@coxautoinc.com>
2020-11-19 11:53:19 +0000

    no message

---
+++ XwDiskSpace/Main.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace XwDiskSpace
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textStartPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.listViewResult = new System.Windows.Forms.ListView();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textStartPath
+            // 
+            this.textStartPath.Location = new System.Drawing.Point(49, 11);
+            this.textStartPath.Margin = new System.Windows.Forms.Padding(2);
+            this.textStartPath.Name = "textStartPath";
+            this.textStartPath.Size = new System.Drawing.Size(824, 20);
+            this.textStartPath.TabIndex = 0;
+            this.textStartPath.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path:";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(878, 10);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(24, 22);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            // 
+            // listViewResult
+            // 
+            this.listViewResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewResult.GridLines = true;
+            this.listViewResult.HideSelection = false;
+            this.listViewResult.Location = new System.Drawing.Point(12, 151);
+            this.listViewResult.MultiSelect = false;
+            this.listViewResult.Name = "listViewResult";
+            this.listViewResult.Size = new System.Drawing.Size(890, 554);
+            this.listViewResult.TabIndex = 7;
+            this.listViewResult.UseCompatibleStateImageBehavior = false;
+            this.listViewResult.View = System.Windows.Forms.View.Details;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLog.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLog.Location = new System.Drawing.Point(15, 38);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(887, 66);
+            this.textBoxLog.TabIndex = 8;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(755, 110);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(147, 23);
+            this.buttonCalculate.TabIndex = 9;
+            this.buttonCalculate.Text = "Calculate Space";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 717);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.listViewResult);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textStartPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XwDiskSpace";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textStartPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.ListView listViewResult;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ XwDiskSpace/Main.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XwDiskSpace
+{
+    public partial class Main : Form
+    {
+        private ImageList imageList = new ImageList();
+        private Dictionary<string, long> FolderSizes = new Dictionary<string, long>();
+        private Stopwatch runTime = new Stopwatch();
+        
+        //*************************************************************************************************************
+        public Main()
+        {
+            //            imageList.Images.Add(global::XwNoNagle.Properties.Resources.nic);   //0
+
+            InitializeComponent();
+
+            string CurrentVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(
+               System.Reflection.Assembly.GetAssembly(typeof(Main)).Location).FileVersion.ToString();
+            Text = $"XwDiskSpace v{CurrentVersion}";
+        }
+
+        //*************************************************************************************************************
+        private void Main_Load(object sender, EventArgs e)
+        {
+#if DEBUG
+            textStartPath.Text = @"C:\data";
+#endif
+
+            listViewResult.SmallImageList = imageList;
+            listViewResult.FullRowSelect = true;
+            listViewResult.Columns.Add("name");
+            listViewResult.Columns.Add("zone");
+            listViewResult.Columns.Add("type");
+            listViewResult.Columns.Add("value");
+            Main_Resize(sender, e);
+        }
+
+        //*************************************************************************************************************
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if(listViewResult.Columns.Count == 0)
+                return;
+
+            listViewResult.Columns[0].Width = 200;
+            listViewResult.Columns[1].Width = 200;
+            listViewResult.Columns[2].Width = 100;
+            listViewResult.Columns[3].Width = listViewResult.Width - 20 - 500;
+        }
+
+        //*************************************************************************************************************
+        private void AddLog(string log)
+        {
+            textBoxLog.AppendText(log + "\r\n");
+            Refresh();
+        }
+
+        //*************************************************************************************************************
+        private long ProcessFolder(string path, int level = 0)
+        {
+            long folderSize = 0;
+            DirectoryInfo root = null;
+
+            try
+            {
+                root = new DirectoryInfo(path);
+                var objs = root.EnumerateFileSystemInfos();
+                foreach (var o in objs)
+                {
+                    if (o.Attributes.HasFlag(FileAttributes.Directory))
+                    {
+                        folderSize += ProcessFolder(o.FullName, level + 1);
+                    }
+                    
+                    if (o.Attributes.HasFlag(FileAttributes.Archive))
+                    {
+                        folderSize += ((FileInfo)o).Length;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                BeginInvoke((Action)(() =>
+                {
+                    AddLog(ex.Message);
+                }));
+            }
+
+            /*
+            try
+            {
+                root = new DirectoryInfo(path);
+                var files = root.EnumerateFiles();
+                foreach (FileInfo file in files)
+                {
+                    try
+                    {
+                        folderSize += file.Length;
+                    }
+                    catch (Exception ex)
+                    {
+                        AddLog(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLog(ex.Message);
+            }
+            
+            try
+            { 
+                var folders = root.EnumerateDirectories();
+                foreach (DirectoryInfo folder in folders)
+                {
+                    folderSize += ProcessFolder(folder.FullName, level+1);
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLog(ex.Message);
+            }
+            */
+
+            if (level == 1)
+                FolderSizes.Add(path, folderSize);
+            
+            return folderSize;
+        }
+        
+        //*************************************************************************************************************
+        private string GetFileSize(double byteCount)
+        {
+            string size = "0 Bytes";
+            if (byteCount == 0)
+                size = "0 B";
+            else if (byteCount >= (1024 * 1024 * 1024))
+                size = String.Format("{0:##.00}", byteCount / (1024 * 1024 * 1024)) + " Gb";
+            else if (byteCount >= (1024 * 1024))
+                size = String.Format("{0:##.00}", byteCount / (1024 * 1024)) + " Mb";
+            else if (byteCount >= 1024)
+                size = String.Format("{0:##.00}", byteCount / 1024) + " Kb";
+            else if (byteCount < 1024)
+                size = String.Format("{0:##}", byteCount) + " B";
+
+            return size;
+        }
+
+        //*************************************************************************************************************
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Text = "";
+            listViewResult.Items.Clear();
+            FolderSizes.Clear();
+
+            AddLog("Running...");
+            runTime.Start();
+
+            Task.Run( () =>
+            {
+                ProcessFolder(textStartPath.Text);
+                var ordered = FolderSizes.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+                int top = 500;
+                foreach (var f in ordered)
+                {   if (top-- == 0)
+                        break;
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = 0;
+                    item.Text = f.Key;
+                    item.SubItems.Add(GetFileSize(f.Value));
+
+                    BeginInvoke((Action)(() =>
+                    {
+                        listViewResult.Items.Add(item);
+                    }));
+                }
+
+                BeginInvoke((Action)(() =>
+                {
+                    runTime.Stop();
+                    AddLog(runTime.Elapsed.ToString());
+                    AddLog("========== DONE ===========");
+                }));
+            });
+        }
+    }
+}
